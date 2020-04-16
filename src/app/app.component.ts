0f9563c629c511902fab716bc742636dc2cffc7d
youwin1234 <tebbytoad@gmail.com>
2020-04-16 12:14:40 -0400

    Publishing WikiSearch
    Making my WikiSearch code public

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wikiSearch';
+  pages = [];
+
+  constructor(private wikipedia: WikipediaService){}
+
+  onTermSubmit(term:string) {
+    this.wikipedia.search(term).subscribe((response:any) => {
+      this.pages = response.query.search;
+    });
+  }
 }
